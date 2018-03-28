1b17ed829f979e3d7b2767ab89d63b6c8442ae37
Reazul Islam <reazulislam137@gmail.com>
2018-03-28 14:49:32 +0600

    first commit

---
+++ march-28-03.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int tot_mins, hrs, mins;
+const int MINaHOUR = 60;
+char line_text[50];
+
+int main()
+{
+    printf("Input minutes: ");
+	fgets(line_text, sizeof(line_text), stdin);
+	sscanf(line_text, "%d", &tot_mins);
+
+	hrs = (tot_mins / MINaHOUR);
+	mins = (tot_mins % MINaHOUR);
+
+	printf("%d Hours, %d Minutes.\n", hrs, mins);
+
+	return 0;
+}
